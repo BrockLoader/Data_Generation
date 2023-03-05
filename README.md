2a735e651f8d340ec4a604a5d6c1d47c953b077a
Rock <117951880+BrockLoader@users.noreply.github.com>
2023-03-05 10:05:51 +1000

    all files updated.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Data_Generation
- Data generation for projects.
+
+Examples:
+
+generate_names.py
+
+from generate_names import generate_names
+
+namelist = list()
+
+namelist = generate_names()
+
+This will prompt you for how many names you want to generate, then create a list with the desired number of names filtered out.
---
+++ generate_names.py
@@ -0,0 +1,21 @@
+import names
+
+def generate_names():
+    val = input('How many names do you want to generate?: ')
+
+    val = int(val)
+
+    name_list = list()
+
+    loop_count = 0
+
+    while loop_count < val:
+        full_name = names.get_first_name() + ' ' + names.get_last_name()
+        if full_name in name_list:
+            break
+        name_list.append(full_name)
+        loop_count = loop_count + 1
+    
+    return(name_list)
+
+
